3286e38a8e4f3142050eefadbf82275f1e893051
KardiWeb <kw@kardiweb.org>
2013-02-04 09:27:05 +0100

    Update debian_install.sh
    ISPConfig3 path error

--- debian_install.sh
+++ debian_install.sh
@@ -571,7 +571,7 @@ install_ISPConfig (){
 cd /tmp
 wget http://www.ispconfig.org/downloads/ISPConfig-3-stable.tar.gz
 tar xfz ISPConfig-3-stable.tar.gz
-cd ispconfig3_install/install/
+cd /tmp/ispconfig3_install/install/
 php -q install.php
 
 } 
