94a90bd6a594603c9371e4d9da0827be283a5f68
Drew Clardy <dclardy64@gmail.com>
2013-07-16 11:09:38 -0500

    Changed some install wording

--- debian_install.sh
+++ debian_install.sh
@@ -8,7 +8,7 @@
 
 # Check if user is root
 if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script, please use root to install the software."
+    echo "Error: You must be root to run this script, please use the root user to install the software."
     exit 1
 fi
 
